6bca06cfa85c78da0722dcbb6027b785c656a3df
tranduyfong <tranphong86958@gmail.com>
2024-01-26 11:05:55 +0700

    Slide 30 Chuong 2

--- BaiThucHanh/src/Slide30.java
+++ BaiThucHanh/src/Slide30.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Slide30 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        int n, soDu, sumOfNumbers = 0;
         //Nhap n
-        System.out.print("Nhap so nguyen duong n : ");
-        int n = input.nextInt();
-        int sumOfNumbers = 0;
-        int soDu;
+        do {
+            System.out.print("Nhap so nguyen duong n : ");
+            n = input.nextInt();
+        } while(n <= 0);
         while (n > 0) {
             soDu = n % 10;
             n /= 10;

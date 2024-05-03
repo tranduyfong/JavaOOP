2010208f7011bd92537e5a5269277c5ca070ca78
tranduyfong <tranphong86958@gmail.com>
2024-05-03 09:56:51 +0700

    Slide 22 Chuong 4

---
+++ BaiThucHanh/src/Slide22Chuong4.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Slide22Chuong4 {
+    public static void main(String[] args) {
+        // Viet chuong trinh thu mot so phuong thuc cua arrayList
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Red");
+        arrayList.add("Blue");
+        arrayList.add("Pink");
+        arrayList.remove("Pink");
+        arrayList.add("Yellow");
+
+        System.out.println(arrayList.size());
+        System.out.println(arrayList.contains("Red"));
+        System.out.println(arrayList);
+    }
+}

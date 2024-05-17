25b5c05fb67da61c907b924649cc99f624760b2a
tranduyfong <tranphong86958@gmail.com>
2024-05-17 09:24:14 +0700

    Slide 89 Chuong 4

--- BaiThucHanh/src/Chuong4/Slide89Chuong4.java
+++ BaiThucHanh/src/Chuong4/Slide89Chuong4.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class Slide98Chuong4 {
+public class Slide89Chuong4 {
     public static void main(String[] args) {
         HashMap<String, String> hashMapCity = new HashMap<>();
         hashMapCity.put("QN", "Quang Nam");
